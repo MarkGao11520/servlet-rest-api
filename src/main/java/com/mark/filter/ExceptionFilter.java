8b7b1d2f9f535fba5835ec101c196166e34e97d5
gwf <18722042639@163.com>
2017-07-08 10:30:54 +0800

    异常及返回结果封装

---
+++ src/main/java/com/mark/filter/ExceptionFilter.java
@@ -0,0 +1,55 @@
+package com.mark.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.mark.core.Result;
+import com.mark.core.ResultCode;
+import com.mark.core.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by gaowenfeng on 2017/7/8.
+ */
+@WebFilter(urlPatterns = "/*")
+public class ExceptionFilter implements Filter {
+    private final Logger logger = LoggerFactory.getLogger(ExceptionFilter.class);
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        response.setCharacterEncoding("utf-8");
+        Result result = new Result();
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            if(e instanceof ServiceException){//如果是你定义的业务异常
+                System.out.println("1");
+                request.setAttribute("ServiceException", e);//存储业务异常信息类
+                result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                logger.info(e.getMessage());
+            }else {
+                result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
+                String message = String.format("接口 [%s] 出现异常，异常摘要：%s",
+                        request.getRequestURI(),
+                        e.getMessage());
+                logger.error(message, e);
+            }
+            System.out.println(JSON.toJSONString(result));
+            response.getWriter().append(JSON.toJSONString(result));
+        }
+    }
+
+    public void destroy() {
+
+    }
+}
---
+++ target/servlet-rest-api/error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gaowenfeng
+  Date: 2017/7/8
+  Time: 上午12:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>错误</title>
+</head>
+<body>
+您访问的界面不存在
+</body>
+</html>
